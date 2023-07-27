ea2cf701e9651a0756fbdd40c4638611bedbe911
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-27 18:03:19 +0300

    redo

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const container = document.getElementById('container');
-console.log(container);
+// console.log(container);
 
-
+let n = 1;
 for (let i = 0; i < 256; i++)
 {
+   
     const div = document.createElement('div');
-    div.className = 'grid';
-    div.style.width = "80px";
-    div.style.height = "80px";
-    div.style.backgroundColor = "red";
-    div.style.border = "2px solid black";
-    div.style.margin = "1px";
+    div.className = 'grid-cell';
+    div.textContent = n;
     container.appendChild(div);
+    n++;
 }
 
+const button = document.querySelector('button');
+const userResponseWidth = prompt('Enter the width of the grid (<=100)');
+
+if (userResponseWidth !== null && userResponseWidth > 0 && userResponseWidth <= 100)
+{
+
+}
+
+const changeGrid = () => {
+    = userResponse;
+}
+
+button.addEventListener('click', userResponseWidth)
